cd4662ce3e387431651029cfbad714676d69cadb
iryna-kashantsova <iryna.kashantsova@ventionteams.com>
2024-09-11 10:41:28 +0500

    feat: do task

---
+++ index.ts
@@ -0,0 +1,19 @@
+interface CartItem {
+    name: string;
+    price: number;
+  }
+  
+const shoppingCart: CartItem[] = [];
+
+function addItem(name: string, price: number): string {
+    const newItem: CartItem = { name, price };
+    shoppingCart.push(newItem);
+    return `${name} has been added to the cart.`;
+}
+
+function calculateTotalCost(items: CartItem[]): number {
+    return items.reduce((total, item) => total + item.price, 0);
+}
+  
+
+  
